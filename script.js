55ea3a9759ccd3e94b3797b014c2cb29b6bbbb4d
LiCern <lisacernilogar@me.com>
2020-07-08 23:11:39 +0200

    commented code

--- script.js
+++ script.js
@@ -23,16 +23,16 @@ const equals = document.getElementById('equals');
 
 let display = document.getElementById('display');
 
-let operatorIndex = -1;
+let operatorIndex = -1;             //set this variable at -1 to facilitate the inital use of consecutiveZeroCheck
 
-let disableDecimal = false; 
+let disableDecimal = false;         // this variable will disable the decimal button when true
 
-const clearDisplay = () => {
-    display.innerHTML = "0";
-    disableDecimal = false;
+const clearDisplay = () => {        //reset calculator upon hitting clear
+    display.innerHTML = "0";        //reset display
+    disableDecimal = false;     
 }
 
-const displayButton = (event) => {
+const displayButton = (event) => {   
     if (disableDecimal === true && event.target === decimal) {
         return;
     }
@@ -46,8 +46,9 @@ const displayButton = (event) => {
 }
 
 const consecutiveOperatorCheck = () => {
+    //in case of a division or multiplication with a negative number it just returns so to allow that operation
     if (event.target === subtract && (display.innerHTML.endsWith('/') || display.innerHTML.endsWith('*'))) {
-        return;
+        return;         
     }
     else if (display.innerHTML.endsWith('+') || 
     display.innerHTML.endsWith('-') || 
@@ -63,26 +64,26 @@ const consecutiveOperatorCheck = () => {
 }
 
 const consecutiveZeroCheck = () => {
-    if (display.innerHTML[operatorIndex + 1] == "0" && display.innerHTML[operatorIndex + 2] !== ".") {
-        console.log('hi');
-        display.innerHTML = display.innerHTML.slice(0, operatorIndex + 1) + display.innerHTML.slice(-1);
+    if (display.innerHTML[operatorIndex + 1] == "0" && display.innerHTML[operatorIndex + 2] !== ".") {      //checks for number starting with 0. (excludes 0.x decimals)
+        display.innerHTML = display.innerHTML.slice(0, operatorIndex + 1) + display.innerHTML.slice(-1);    //remove the zero at start of number
     }
 }
 
-const setOperatorIndex = () => {
+const setOperatorIndex = () => {        // logs the position of most recent operator for use elsewhere
     operatorIndex = display.innerHTML.length - 1;
 }
 
-const regex = /[0-9.]+|[+-/*]/g;
+const regex = /[0-9.]+|[+-/*]/g;    //regex to match numbers OR operators 
 
-let equationArray;
-console.log(equationArray);
+let equationArray;  //initialise (will update upon hitting equals). This will contain the display equation broken up into array elements.
 
-const calculateMultiplication = () => {
-    for (let i = 0; i < equationArray.length; i++) {
+// main calculation equations
+
+const calculateMultiplication = () => {   
+    for (let i = 0; i < equationArray.length; i++) { // search for operator
         if (equationArray[i] === '*') {
-            equationArray.splice(i-1, 3, equationArray[i-1] * equationArray[i+1])
-            i--;
+            equationArray.splice(i-1, 3, equationArray[i-1] * equationArray[i+1]) // replace adjacent elements with a single element containing result
+            i--;    // to ensure we dont skip an element due to splice reducing number of elements
         }
     }
 };
@@ -99,7 +100,7 @@ const calculateDivision = () => {
 const calculateAddition = () => {
     for (let i = 0; i < equationArray.length; i++) {
         if (equationArray[i] === '+') {
-            equationArray.splice(i-1, 3, parseInt(equationArray[i-1], 10) + parseInt(equationArray[i+1], 10));
+            equationArray.splice(i-1, 3, parseInt(equationArray[i-1], 10) + parseInt(equationArray[i+1], 10)); //parseInt() stops concatenation
             i--;
         }
     }
@@ -115,7 +116,7 @@ const calculateSubtraction = () => {
 };
 
 const completeCalculation = () => {
-    equationArray = display.innerHTML.match(regex);
+    equationArray = display.innerHTML.match(regex);     //use regex previously defined to get an array of alternating numbers and operators 
     console.log(equationArray);
     calculateMultiplication();
     console.log(equationArray);
@@ -125,7 +126,7 @@ const completeCalculation = () => {
     console.log(equationArray);
     calculateAddition();
     console.log(equationArray);
-    display.innerHTML = equationArray.join("");
+    display.innerHTML = equationArray.join("");         //equationArray to turn into a string to be displayed as the final result
 };
 
 
